42f05e664df3e5e498b22372b86cc37fa807f4d0
TheBawawa <anagabrielaparedes324@gmail.com>
2022-09-07 12:29:27 -0500

    Lab 1

--- Other Exercises/Lab1.js
+++ Other Exercises/Lab1.js
@@ -1,13 +1,5 @@
 var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
 
-//Task 1
-function logDairy() {
-  for (var i = 0; i <dairy.length; i++) {
-    console.log (dairy[i]);
-  }
-}
-//logDairy()
-
 const animal = {
   canJump: true
   }; 
@@ -16,12 +8,26 @@ const bird = Object.create(animal);
 bird.canFly = true;
 bird.hasFeathers = true;
 
-//Task 2
-function birdCan() {
-  for ( const key in bird) {
-    console.log(`${key}: ${bird[key]}`); 
+//Task 1
+function logDairy() {
+  for (var i = 0; i <dairy.length; i++) {
+    console.log (dairy[i]);
   }
 }
-birdCan()
+//logDairy()
+
+//Task 2
+function birdCan() {
+  for ([x,y] of Object.entries(bird)) {
+    console.log(`${x}: ${y}`);
+  }
+}
+//birdCan()
 
 //Task 3
+function animalCan() {
+  for ( const key in bird) {
+    console.log(`${key}: ${bird[key]}`);
+  }
+}
+//animalCan()
