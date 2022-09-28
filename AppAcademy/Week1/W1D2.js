d593b7c91e0a409c8ee7726b8c502cc1e3c5d60f
TheBawawa <anagabrielaparedes324@gmail.com>
2022-09-27 22:50:35 -0500

    W1D2

--- AppAcademy/Week1/W1D2.js
+++ AppAcademy/Week1/W1D2.js
@@ -94,38 +94,27 @@ function whisper(string) {
 // `subString`. The function should return `true` if `subString` is a part of the
 // `searchString`, `false` otherwise.
 // Write two versions of this function, using conditionals and without using conditionals
-//
-// Examples:
-// isSubstring("The cat went to the store", "he cat went"); // => true
-// isSubstring("Time to program", "time"); // => true
-// isSubstring("Jump for joy", "joys"); // => false
-//
+
+//1
 function isSubstring(searchString, subString) {
-    searchString = searchString.toLowerCase();
-    subString = subString.toLowerCase();
-
-  let listwords = searchString.split('');
-  let sublist = subString.split('');
-  let answer = false;
-
-  let sublength = sublist.length;
-  let listlength = listwords.length;
-
-  for (let i = 0; i < sublength; i++) {
-    for (let j = 0; j < listlength; j++) {
-        if (listwords[i] === sublist[j]){
-            answer = true;
-        }
-    }
+  if (searchString.toLowerCase().indexOf(subString.toLowerCase()) > -1){
+    console.log(true);
   }
-  console.log(answer);
-  console.log(listwords);
-  console.log(sublist);
+  else {
+    console.log(false)
+  }
 }
-isSubstring("Jump for joy", "joys");
+//isSubstring("Jump for joy", "joys"); 
+//isSubstring("Time to program", "time");
+
+//2
+function isSubstringV2(searchString, subString){
+  return (searchString.toLowerCase().indexOf(subString.toLowerCase()) > -1)
+}
+//console.log(isSubstringV2("Time to program", "time"));
+//console.log(isSubstringV2("Jump for joy", "joys"));
 
 //echo
-
 // Write a function `echo` that takes in a string and returns that string "echo-ized".
 //
 // Examples:
@@ -134,39 +123,42 @@ isSubstring("Jump for joy", "joys");
 // echo("JUMp"); // => "JUMP ... JUMp ... jump"
 //
 function echo(string) {
-  // your code here...
-}
+  let caps = string.toUpperCase();
+  let lower = string.toLowerCase();
 
+  console.log(`${caps} ... ${string} ... ${lower}`);
+}
+//echo("JUMp");
+//echo("Mom!");
 
 //isEven
 
 // Write a function `isEven` that takes in a number and returns `true` if the number
 // is even, `false` otherwise. Write `isEven` by using the `isOdd` function you
 // made previously. HINT: you can call `isOdd` from within `isEven`.
-//
-// Examples:
-// isEven(2); // => true
-// isEven(5); // => false
 function isEven(num) {
-  // your code here...
+  if (num % 2 == 0) {
+    console.log(true);
+  }
+  else {
+    console.log(false);
+  }
 }
-
+//isEven(2);
+//isEven(5);
 
 //averageOfFour
-
-
 // Write a function `averageOfFour(num1, num2, num3, num4)` that takes in four
 // numbers. The function should return the average of all of the numbers.
-//
-// Examples:
-//
-// averageOfFour(10, 10, 15, 5); // => 10
-// averageOfFour(3, 10, 11, 4); // => 7
-// averageOfFour(1, 2, 3, 4); // => 2.5
 
+function averageOfFour(num1, num2, num3, num4){
+  let average = (num1 + num2 + num3 + num4) / 4;
+  console.log(average)
+}
+//averageOfFour(1, 2, 3, 4);
+//averageOfFour(3, 10, 11, 4);
 
 //Function Muscle Memory
-
 //Practice typing the syntax for functions. **Type each snippet 10 times.** Focus on using
 //proper style and indentation. Be sure to test your code by running it after
 //each repetition. You'll be surprised and also annoyed at what details you missed!
@@ -178,7 +170,70 @@ function sayHello(name) {
   return msg;
 }
 
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
+
+function sayHello(name) {
+  var msg = 'Hello, ' + name + '. How are you?';
+  return msg;
+}
 //console.log(sayHello('bootcamp prep')); // feel free to change the string being passed
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
 
 
 function checkNumber(num) {
@@ -191,13 +246,89 @@ function checkNumber(num) {
   }
 }
 
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
+
+function checkNumber(num) {
+  if (num > 0) {
+    return 'positive';
+  } else if (num < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+}
 //console.log(checkNumber(5)); // feel free to change the number being passed
 
-
 //Thinking Problems
-
 //These next few problems won't require you to write code, but it will help your understandng of JavaScript.
 
 //Order of Operations
-
 //Evaluate each expression. Check your answer in the Node REPL.
+
+`1 + 1 * 5` //7
+
+`(1 + 1) * 5` //10
+
+`1 + 2 - 5 / 6 - 1` //1.167
+
+`5 * 5 % 13` //4
+
+`5/(-1 * (5 - (-10)))` //-0.33
